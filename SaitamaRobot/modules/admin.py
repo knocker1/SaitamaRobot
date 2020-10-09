86428d2a5fcd4d8d1ce0fc4070baf5c17de8b409
knocker1 <71930795+knocker1@users.noreply.github.com>
2020-10-09 18:00:01 +0530

    Update admin.py

--- SaitamaRobot/modules/admin.py
+++ SaitamaRobot/modules/admin.py
@@ -391,9 +391,9 @@ dispatcher.add_handler(PROMOTE_HANDLER)
 dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 
-__mod_name__ = "Admin"
-__command_list__ = ["adminlist", "admins", "invitelink", "promote", "demote"]
-__handlers__ = [
+#__mod_name__ = "Admin"
+#__command_list__ = ["adminlist", "admins", "invitelink", "promote", "demote"]
+#__handlers__ = [
     ADMINLIST_HANDLER, PIN_HANDLER, UNPIN_HANDLER, INVITE_HANDLER,
     PROMOTE_HANDLER, DEMOTE_HANDLER, SET_TITLE_HANDLER
-]
+#]
