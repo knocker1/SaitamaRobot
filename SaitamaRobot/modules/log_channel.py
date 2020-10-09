34cf3d7130d8ae1f397932a90b2d478e4515c924
Devp73 <52715880+Devp73@users.noreply.github.com>
2020-10-09 23:25:09 +0530

    Update log_channel.py

--- SaitamaRobot/modules/log_channel.py
+++ SaitamaRobot/modules/log_channel.py
@@ -185,7 +185,9 @@ if is_module_loaded(FILENAME):
             return f"This group has all it's logs sent to: {escape_markdown(log_channel_info.title)} (`{log_channel}`)"
         return "No log channel is set for this group!"
 
-    __help__ = """
+
+
+"""__help__ = """
 *Admins only:*
 • `/logchannel`*:* get log channel info
 • `/setlog`*:* set the log channel.
@@ -195,6 +197,7 @@ Setting the log channel is done by:
 • adding the bot to the desired channel (as an admin!)
 • sending `/setlog` in the channel
 • forwarding the `/setlog` to the group
+"""
 """
 
     #__mod_name__ = "Log Channels"
