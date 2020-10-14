75208ce30fedcf86ec51fb83420d65c64b1a2829
knocker1 <71930795+knocker1@users.noreply.github.com>
2020-10-15 01:04:45 +0530

    Update tts.py

--- SaitamaRobot/modules/tts.py
+++ SaitamaRobot/modules/tts.py
@@ -1,5 +1,8 @@
+import gTTs
+import telegram
+import tg_bot
 from telegram import ChatAction
-#from gtts import gTTS
+from gtts import gTTS
 import html
 import urllib.request
 import re
@@ -12,7 +15,7 @@ from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram import ParseMode
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
-#from tg_bot import dispatcher
+from tg_bot import dispatcher
 from tg_bot.__main__ import STATS
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.extraction import extract_user
@@ -23,7 +26,7 @@ def tts(bot: Bot, update: Update, args):
     reply = " ".join(args)
     update.message.chat.send_action(ChatAction.RECORD_AUDIO)
     lang="ml"
-    tts = (reply, lang)
+    tts = gTTs(reply, lang)
     tts.save("k.mp3")
     with open("k.mp3", "rb") as f:
         linelist = list(f)
@@ -31,12 +34,12 @@ def tts(bot: Bot, update: Update, args):
     if linecount == 1:
         update.message.chat.send_action(ChatAction.RECORD_AUDIO)
         lang = "en"
-        tts = (reply, lang)
+        tts = gTTs(reply, lang)
         tts.save("k.mp3")
     with open("k.mp3", "rb") as speech:
         update.message.reply_voice(speech, quote=False)
 
-#dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
+dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
 
 __help__ = """
 *turns your text to audio format(beta phase)*
